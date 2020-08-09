1d35ae6aebcafc495fa09d163b4846e98dd1203c
romlel35 <romlel35@gmail.com>
2020-08-09 14:01:34 +0200

    heroku test 3 : on mets la route de test que dans la connection à la bdd

--- server.js
+++ server.js
@@ -28,12 +28,7 @@ const PORT = 8000;
 app.listen(process.env.PORT || 8000, ()=>{
 	console.log('listening port '+PORT+' all is ok');
 })
- //route basique de test
- app.get("/",  async (req, res, next)=>{
-                    
-                
-    res.json({ status: 200, msg:"ok google" })
-});
+ 
 
 
 
